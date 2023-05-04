4dbc3102836eed969db5ed93cfa1e5d2dd2f9b42
oumytoure <toureoumy12@gmail.com>
2023-05-04 00:26:43 +0000

    ajout

---
+++ BACKEND/server.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+// Express APIs
+const api = require('./controllers/user.ctrl')
+
+//const app_io = require('./arduino')
+
+mongoose
+  .connect('mongodb+srv://oumy:1234@cluster0.ayfcz7h.mongodb.net/maison')
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch((err) => {
+    console.error('Error connecting to mongo', err.reason)
+  })
+
+
+
+
+
+//formatage datas 
+const app= express()
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+)
+app.use(cors())
+
+// Serve static resources
+app.use('/api', api)
+
+// Error favicon.ico
+app.get('/favicon.ico', (req, res) => res.status(204))
+
+// Define PORT
+const port = process.env.PORT || 4002
+
+const servers = app.listen(port, () => {
+  console.log('Connected to port ' + port)
+})
+
+// Express error handling
+app.use((req, res, next) => {
+  setImmediate(() => {
+    next(new Error('Something went wrong'))
+  })
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err.message)
+  if (!err.statusCode) err.statusCode = 500
+  res.status(err.statusCode).send(err.message)
+})
+
+
+
+
+
