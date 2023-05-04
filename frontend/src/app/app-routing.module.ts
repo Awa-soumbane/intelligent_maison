5b71b303acb4f0576c96d7c982f65c26487c71c4
awa <awasoumbane281@gmail.com>
2023-05-04 15:43:21 +0000

    dashbord

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { TableActifsComponent } from './table-actif/table-actif.component';
-
 import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
 const routes: Routes = [
   {component: InscriptionComponent, path:'inscription'},
   {component: TableActifsComponent, path:'tableActif'},
-
+  {component:SidebarComponent, path:'home'},
   {path:"login", component: LoginComponent},
 
 ];
