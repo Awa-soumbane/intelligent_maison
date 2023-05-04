b0328ad3ddd36a52881559a749d6c4d7b766b8d2
awa <awasoumbane281@gmail.com>
2023-05-04 15:55:20 +0000

    ok

--- frontend/src/app/dashbord/dashbord.component.ts
+++ frontend/src/app/dashbord/dashbord.component.ts
@@ -1,6 +1,4 @@
-
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashbord',
@@ -8,24 +6,5 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./dashbord.component.css']
 })
 export class DashbordComponent {
-  registerForm!: FormGroup
-  onCode(){
-    console.log(this.registerForm.value.codeAccess);
-    
-    /*  if(this.registerForm.value.codeAccess == 7890){
-    this.socket.emit("openDoor", 1);
-    localStorage.setItem('door', '1')
-    this.toastr.info('Porte ouverte')
-   }else if(this.registerForm.value.codeAccess == 9078){
-    this.socket.emit("closeDoor", 0);
-    localStorage.setItem('door', '0')
-    this.toastr.info('Porte fermée')
-   }else{
-    this.toastr.error('Code d\'accès incorrect')
-   }		
-   */
-  /*  setTimeout(() => {
-    this.ngOnInit()
-   }, 2000); */
-  }
+
 }
